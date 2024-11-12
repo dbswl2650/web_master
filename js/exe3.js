e9ccd9f883e212ed0492b511e41e14313b7d4836
pc <pc@email.com>
2024-11-12 23:21:14 +0900

    오늘 지옥이었다!

--- js/exe3.js
+++ js/exe3.js
@@ -2,33 +2,52 @@
 //1.태어난 년도 입력 -> 나이계산.
 //2.직사각형 너비 계산 -> 가로길이, 세로길이 입력.
 //3.최고값, 최소값 -> 입력숫자는 3개.
-let question =1;
-if(question == 1){
-  let age = prompt('태어난 년도 입력');
-  console.log((2024 - age) + "살");
-} else if (question ==2) {
-  let width = 100;
-  let height = 80;
-  let result = 0;
-  console.log(result= width * height);
-} else {
-  let A = prompt('첫번째 숫자');
-  let B = prompt('두번째 숫자');
-  let c = prompt('세번째 숫자');
-  let max = 0, min = 0;
-  if (A < B){
-    if (A < C){
-      if (B < C){
-        max = C;
-        min = A;
-      } else if (C < B){
-        max = B;
-        min = A;
+let question = 1;
+  if (question == 1) {
+    let age = prompt('태어난 년도 입력');
+    console.log((2024 - age) + "살");
+
+} else if (question == 2) {
+    let width = 100;
+    let height = 80;
+    alert('너비' + (width * height));
+
+} else (question ==3)
+    let a = prompt('첫번째 숫자');
+    let b = prompt('두번째 숫자');
+    let c = prompt('세번째 숫자');
+    let max = 0, min = 0;
+
+    if (a > b) {
+      if (b > c) {
+        max = a;
+        min = c;
       }
-    }else if (A > C){
-      max = B;
-      min = C;
+      else if (c > b) {
+        if (a > c) {
+          max = a;
+          min = b;
+       }
+        else if (c > a) {
+         max = c;
+          min = b;
+       }
+      }
     }
-}else if (A > B) {
-  if()
-}
+    else if (b > a) {
+     if (a > c) {
+        max = b;
+       min = c;
+      }
+      else if (c > a) {
+       if (b > c) {
+          max = b;
+          min = a;
+        }
+        else if (c > b) {
+          max = c;
+          min = a;
+        }
+      }
+    }
+    console.log('젤 큰숫자 ' + max + ' 젤 작은숫자 ' + min);
