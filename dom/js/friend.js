60d5803df96081e52898b57e3c83a0af272d202c
pc <pc@email.com>
2024-11-19 17:46:16 +0900

    진짜 못알아듣겠다

--- dom/js/friend.js
+++ dom/js/friend.js
@@ -4,6 +4,7 @@ function makeRow(friendInfo = {name, phone, birth, blood}) {
   // 이름, 연락처, 생일. => tr 생성한 후 tr반환.
   let tr = document.createElement('tr');
   tr.addEventListener('click', function(e) {
+    e.stopPropagation();
     console.log(this.children[0].innerHTML)
     document.querySelector('input[name="friendName"]').value = this.children[1].innerHTML
     document.querySelector('input[name="friendPhone"]').value = this.children[2].innerHTML
@@ -14,8 +15,31 @@ function makeRow(friendInfo = {name, phone, birth, blood}) {
   // <td><input type = "checkbox"></td>
 
   let td = document.createElement('td');
-  let cd = document.createElement('input')
+  let cd = document.createElement('input');
   cd.setAttribute("type", "checkbox");
+  cd.addEventListener('click', (e) => e.stopPropagation());
+  cd.addEventListener('change', (e) => {
+  //   document.querySelector('thead input[type="checkbox"]').checked = true;
+  //   document.querySelectorAll('tbody input[type="checkbox"]').forEach(item => {
+  //     if(!item.checked) {
+  //       document.querySelector('thead input[type="checkbox"]').checked = false
+  //     }
+  //   })
+  // }) // 첫번째로 해본거
+
+  let chks = document.querySelectorAll('tbody input[type="checkbox"]');
+  let chkeds = document.querySelectorAll('tbody input[type="checkbox"]:checked');
+  console.log(chks.length, chkeds.length);
+  document.querySelector('thead input[type="checkbox"]').checked = 
+    chks.length == chkeds.length ? true : false; // 3번째로 해본거
+
+  // if(chks.length == chkeds.length) {
+  //   document.querySelector('thead input[type="checkbox"]').checked = true;
+  //   }
+  //   else {
+  //     document.querySelector('thead input[type="checkbox"]').checked = false
+  //   }
+}) // 2번째로 해본거
   td.appendChild(cd);
   tr.appendChild(td);
 
@@ -30,12 +54,15 @@ function makeRow(friendInfo = {name, phone, birth, blood}) {
   btn.innerHTML = '삭제'
   btn.setAttribute('class', 'btn btn-danger'); // 태그에 attribute를 추가.
   btn.addEventListener('click', (e) => { // 삭제버튼에 클릭이벤트 등록.
+    e.stopPropagation();
     e.target.parentElement.parentElement.remove();
   })
   td.appendChild(btn);
   tr.appendChild(td);
   return tr;
 }
+// end of makerow
+
 
 // data.js
 console.log(friendList);
@@ -108,11 +135,37 @@ document.querySelector('button:nth-of-type(2)').addEventListener('click', (e) =>
 //선택삭제 이벤트.
 document.querySelector('button.btn.btn-danger').addEventListener('click', (e) => {
   // #list input[type="checkbox"]
-  document.querySelectorAll('#list tr').forEach((item) =>{
+  document.querySelectorAll('#list tr').forEach((item) => {
     if(item.children[0].children[0].checked) {
-      item.remove();
+      item.remove()
     }
-  });
+  }) 
 }); // end of 선택삭제.
 
 // 전체선택 하는 이벤트 추가. change 이벤트.
+document.querySelector('thead input[type="checkbox"]').addEventListener('change', (e) => {
+  console.log(e.target.checked); // input:checkbox => checked 속성.
+  //대상변경: tbody input[type="checkbox"]
+  document.querySelectorAll('tbody input[type = "checkbox"]').forEach((item) => {
+    item.checked = e.target.checked;
+  });
+})
+//  정보저장버튼 클릭하면 친구의 정보를 localStorage에 저장.
+ document.querySelector('button.btn-info').addEventListener('click', e => {
+  let ary = []
+  document.querySelectorAll('#list tr').forEach(item => {
+    console.log(item);
+    let name =item.children[1].innerHTML; // 이름.
+    let phone = item.children[2].innerHTML; // 연락처.
+    let birth = item.children[3].innerHTML; // 생일.
+    let btype = item.children[4].innerHTML; // 혈액형.
+    let obj = {name, phone, birth, btype}
+    console.log(obj);
+    ary.push(obj);
+  });
+  console.log(ary);
+  let json = JSON.stringify(ary);
+  localStorage.setItem('friendList', ary);
+ })
+
+ //
